99ef063ce2f5f64d6dd835069ef91dd1e1cb3075
pseudomuto <david.muto@gmail.com>
2015-08-29 11:37:11 -0400

    extracting ResourceActions

--- test-env.js
+++ test-env.js
@@ -1,2 +1,6 @@
 window.fetch = require("whatwg-fetch");
 console.warn = jest.genMockFunction();
+
+window.stubFetchRequest = (api, json) => {
+  api.get.mockReturnValue(new Promise((resolve, _) => resolve(json)));
+};
