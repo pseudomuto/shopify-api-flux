5a16988cb25590223bfc1b45f3e15dbc481a50ef
pseudomuto <david.muto@gmail.com>
2015-09-12 12:48:39 -0400

    removing some unnecessary methods

--- test-env.js
+++ test-env.js
@@ -1,10 +1,6 @@
 window.fetch = require("whatwg-fetch");
 console.warn = jest.genMockFunction();
 
-window.stubFetchRequest = (api, json) => {
-  stubRequest(api, "get", json);
-};
-
 window.stubRequest = (api, method, json) => {
   api[method].mockReturnValue(new Promise((resolve, _) => resolve(json)));
 };
