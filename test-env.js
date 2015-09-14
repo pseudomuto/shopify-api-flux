f7fce3185cb71e91196bc0eca09448726c5fd71d
pseudomuto <david.muto@gmail.com>
2015-09-14 09:19:39 -0400

    adding integration tests for read operations

--- test-env.js
+++ test-env.js
@@ -4,3 +4,16 @@ console.warn = jest.genMockFunction();
 window.stubRequest = (api, method, json) => {
   api[method].mockReturnValue(new Promise((resolve, _) => resolve(json)));
 };
+
+window.setupIntegrationTest = (session) => {
+  window.fetch = require("fetcher").fetch;
+
+  require("node-env-file")(".env");
+
+  beforeEach(() => {
+    session.init(
+      process.env.SHOPIFY_API_DOMAIN,
+      process.env.SHOPIFY_API_TOKEN
+    );
+  });
+};
